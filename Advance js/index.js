6b8922c546cec8d5b211a61e7f91c3efb31b2394
Prem Singh <hunny345686@gmail.com>
2026-06-22 06:12:24 +0530

    Learning - about Promises how it works

--- Advance js/index.js
+++ Advance js/index.js
@@ -36,3 +36,41 @@ function throtel(fn, delay) {
     }
 }
 
+// Promiss ALL polyfill 
+
+function promiseAll(promises) {
+    return new Promise((resolve, reject) => {
+
+        const results = [];
+        let completed = 0;
+
+        if (promises.length === 0) {
+            resolve([]);
+            return;
+        }
+
+        promises.forEach((promise, index) => {
+
+            Promise.resolve(promise)
+                .then(res => {
+
+                    results[index] = res;
+                    completed++;
+
+                    if (completed === promises.length) {
+                        resolve(results);
+                    }
+
+                })
+                .catch(reject);
+        });
+    });
+}
+
+
+promiseAll([
+    Promise.resolve(1),
+    new Promise(r => setTimeout(() => r(2), 1000)),
+    Promise.resolve(3)
+])
+    .then(console.log);
