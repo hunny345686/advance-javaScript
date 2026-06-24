0c5b13747a02514e1e0d6bd8eeb0a066888cf1be
Prem Singh <hunny345686@gmail.com>
2026-06-24 10:15:28 +0530

    Learning Deep clone ,event Emiter

--- Advance js/index.js
+++ Advance js/index.js
@@ -74,3 +74,92 @@ promiseAll([
     Promise.resolve(3)
 ])
     .then(console.log);
+
+
+// Debounce 
+
+function debounce(fn, delay) {
+    let timer
+
+    return function (...args) {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
+
+
+function search(value) {
+    console.log("Searching:", value);
+}
+
+const debouncedSearch = debounce(search, 500);
+
+debouncedSearch("P");
+debouncedSearch("Pr");
+debouncedSearch("Pre");
+debouncedSearch("Prem");
+
+
+// event Emiter
+
+class EventEmitter {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, listener) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+
+        this.events[event].push(listener);
+    }
+
+    emit(event, ...args) {
+        if (!this.events[event]) return;
+
+        this.events[event].forEach(listener => {
+            listener(...args);
+        });
+    }
+
+    off(event, listenerToRemove) {
+        if (!this.events[event]) return;
+
+        this.events[event] =
+            this.events[event].filter(
+                listener => listener !== listenerToRemove
+            );
+    }
+}
+
+const emitter = new EventEmitter();
+
+function greet(name) {
+    console.log(`Hello ${name}`);
+}
+
+emitter.on("welcome", greet);
+
+emitter.emit("welcome", "Prem");
+
+
+// Simple Deep Clone
+
+function deepClone(obj) {
+    if (obj === null || typeof obj !== "object") {
+        return obj;
+    }
+
+    const clone = Array.isArray(obj)
+        ? []
+        : {};
+
+    for (let key in obj) {
+        clone[key] = deepClone(obj[key]);
+    }
+
+    return clone;
+}
