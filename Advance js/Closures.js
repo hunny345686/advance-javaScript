0658d3c0d4388358fd1691f34ab25e6a6bf6c746
Prem Singh <hunny345686@gmail.com>
2026-07-13 12:02:15 +0530

    Clouser consept

--- Advance js/Closures.js
+++ Advance js/Closures.js
@@ -1,12 +1,12 @@
-// Function remembers variables from outer scope even after outer function has finished execution.
+// // Function remembers variables from outer scope even after outer function has finished execution.
 
 function counter() {
-    let count = 0;
+  let count = 0;
 
-    return function () {
-        count++;
-        return count;
-    };
+  return function () {
+    count++;
+    return count;
+  };
 }
 
 const increment = counter();
@@ -14,3 +14,25 @@ const increment = counter();
 console.log(increment());
 console.log(increment());
 console.log(increment());
+
+function createCounter() {
+  let count = 0;
+
+  function increment() {
+    count++;
+
+    if (count < 3) {
+      console.log(count);
+      increment();
+    }
+
+    return count;
+  }
+
+  return increment;
+}
+
+const fn = createCounter();
+
+console.log(fn());
+console.log(fn());
